e8f8d5533b635d3c4c6b7d89929cb2875246a627
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-11-22 18:26:36 -0500

    added shop logo patch endpoint

--- controllers/shopauthcontroller.js
+++ controllers/shopauthcontroller.js
@@ -75,6 +75,45 @@ router.put('/', function(req, res) {
     );
 });
 
+//EDIT SHOP ADD LOGO
+router.patch('/', function(req, res) {
+    let data = req.user.id;
+    // let shopName = req.body.shopdata.shopName;
+    // let payName = req.body.shopdata.payName;
+    // let shopDescription = req.body.shopdata.shopDescription;
+    let logo = req.body.shopdata.logo;
+    // let color1 = req.body.shopdata.color1;
+    // let color2 = req.body.shopdata.color2;
+    // let color3 = req.body.shopdata.color3;
+    // let open = req.body.shopdata.open;
+    var shopdata = req.body.user;
+
+    Shop.update({
+        // userID: userID,
+        // shopName: shopName,
+        // payName: payName,
+        // shopDescription: shopDescription,
+        logo: logo,
+        // color1: color1,
+        // color2: color2,
+        // color3: color3,
+        // open: open
+    },
+    {where: { userID: data }}
+    ).then(
+        function updateSuccess(updateshopdata) {
+            res.json({
+                shopdata: shopdata,
+                message: 'Logo successfully updated.'
+            });
+        },
+        function updateError(err) {
+            res.send(500, err.message);
+            console.log('--UPDATE SHOP ERROR--');
+        }
+    );
+});
+
 //DELETE EXISTING SHOP: tested
 router.delete('/', function(req, res) {
     // let data = req.params.id;
