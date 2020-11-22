099cfb5a6ea224627e310d5e7bcc1b47cabaf87c
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-11-22 11:34:57 -0500

    trying to fix user edit controller

--- controllers/userauthcontroller.js
+++ controllers/userauthcontroller.js
@@ -6,13 +6,15 @@ let bcrypt = require('bcryptjs');
 
 //EDIT USER: tested (but may need to mess with how it finds the user ID)
 router.put('/edit', function (req, res) {
-    let userdata = req.body.userdata;
+    console.log(req.user.id)
+    // let userdatareturn = req.body.user;
     let email = req.body.user.email;
     let pass = req.body.user.password;
     let firstName = req.body.user.firstName;
     let lastName = req.body.user.lastName;
     let data = req.user.id;//edited 11/22 12:25am
     let shopOwner = req.body.user.shopOwner;//added 11/22 12:25am
+    var userdata = req.body.user;
 
     User.update({
         email: email,
@@ -22,9 +24,9 @@ router.put('/edit', function (req, res) {
         shopOwner: shopOwner
     }, { where: {id: data} }
     ).then(
-        function updateSuccess(user) {
+        function updateSuccess(updateduserdata) {
             res.json({
-                user: user,
+                userdata: userdata,
                 message: 'User information updated.',
             });
         },
