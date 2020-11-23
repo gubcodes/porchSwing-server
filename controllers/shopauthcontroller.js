f5f2995065656526c2e91a82cc26f1b5e24d5d94
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-11-23 09:33:34 -0500

    get shop by user id backoffice

--- controllers/shopauthcontroller.js
+++ controllers/shopauthcontroller.js
@@ -115,6 +115,26 @@ router.patch('/', function(req, res) {
     );
 });
 
+//GET SHOP BY USERID FROM TOKEN
+router.get('/', function(req, res) {
+    // let data = req.params.id;
+    let data = req.user.id; //this may need to change, what about others pulling up the shop?
+
+    Shop.findOne({
+        where: { userID: data }
+    }).then(
+        function findOneSuccess(data) {
+            res.json(data);
+        },
+        function findOneError(err) {
+            res.send(500, err.message);
+            console.log('--GET SHOP ERROR--')
+        }
+    );
+});
+
+module.exports = router;
+
 //DELETE EXISTING SHOP: tested
 router.delete('/', function(req, res) {
     // let data = req.params.id;
